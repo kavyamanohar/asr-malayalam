827b6efae5ed05bf9b2a3da19e4548519e51df23
Kavya Manohar <sakhi.kavya@gmail.com>
2021-08-17 21:30:35 +0530

    Update cmd.sh to set the queing system of jobs

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,5 @@
 # Setting local system jobs (local CPU - no external clusters)
-export train_cmd=run.pl
-export decode_cmd=run.pl
+export train_cmd="run.pl --mem 6G"
+export decode_cmd="run.pl --mem 6G"
+export mkgraph_cmd="run.pl --mem 8G"
+export cuda_cmd="run.pl --gpu 1"
